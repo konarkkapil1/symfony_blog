71e9cdef172fe6661718a979276fc64642545c61
konark kapil <konarkkapil@rocketmail.com>
2021-01-12 20:05:50 +0530

    first commit till forms

---
+++ src/Controller/PostController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(PostRepository $postRepo): Response
+    {
+        $posts = $postRepo->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/post/create", name="create_post")
+     * @param Request
+     * @return Response
+     */
+    public function create(Request $req){
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+
+
+
+        $form->handleRequest($req);
+
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+//            dump($post);
+            $em->persist($post);
+            $em->flush();
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+
+
+        return $this->render('post/create.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post")
+     * @param Post
+     */
+    public function show(Post $post): Response{
+        return $this->render('post/post.html.twig', ["post" => $post]);
+    }
+
+
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraint as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $added_on;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAddedOn(): ?String
+    {
+        return $this->added_on;
+    }
+
+    public function setAddedOn(string $added_on): self
+    {
+        $this->added_on = $added_on;
+
+        return $this;
+    }
+}
